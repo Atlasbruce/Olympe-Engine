e690a5bd3e366d5877e81b110c562fe71a8ab663
Atlasbruce <2962180+Atlasbruce@users.noreply.github.com>
2026-08-04 14:48:22 +0200

    update parallax map demo to restore the 2D tiles

--- Gamedata/Levels/parallax_forest.tsx
+++ Gamedata/Levels/parallax_forest.tsx
@@ -1,25 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="parallax_forest" tilewidth="1980" tileheight="1080" tilecount="7" columns="0">
+<tileset version="1.10" tiledversion="1.12.2" name="parallax_forest" tilewidth="65" tileheight="65" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="../Resources/Parallax Layers/2.png" width="1980" height="1080"/>
- </tile>
- <tile id="1">
-  <image source="../Resources/Parallax Layers/3.png" width="1980" height="1080"/>
- </tile>
- <tile id="2">
-  <image source="../Resources/Parallax Layers/4.png" width="1980" height="1080"/>
- </tile>
- <tile id="3">
-  <image source="../Resources/Parallax Layers/5.png" width="1980" height="1080"/>
- </tile>
- <tile id="4">
-  <image source="../Resources/Parallax Layers/6.png" width="1980" height="1080"/>
- </tile>
- <tile id="5">
-  <image source="../Resources/Parallax Layers/7.png" width="1980" height="1080"/>
- </tile>
- <tile id="6">
-  <image source="../Resources/Parallax Layers/8.png" width="1980" height="1080"/>
+ <tile id="1" x="0" y="0" width="65" height="65">
+  <image source="tileset 2D ortho.png" width="261" height="260"/>
  </tile>
 </tileset>
